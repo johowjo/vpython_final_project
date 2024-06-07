a7adca4da9ef7a0c5c011e89b799bf9b8b4d2d99
johowjo <johowjo@gmail.com>
2024-06-07 13:41:28 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from vpython import *
+import numpy as np
+
+def drop_needle():
+    print("a needle is dropped!")
+
+scene = canvas
+
+n = input("number of needles to be dropped:")
+d = input("distance between lines:")
+
+
+print(n);
+print(d);
