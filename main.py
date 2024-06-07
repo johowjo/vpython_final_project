c49796156f8af03302e0846ed32a5ff3aba34304
johowjo <johowjo@gmail.com>
2024-06-07 16:42:15 +0800

    working at bare minimum

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from vpython import *
 
 r1, m1, q1, c1 = 4, 4, 2, color.red
+r2, m2, q2, c2 = 1, 197, 79, color.yellow
 v0 = vec(10, 0, 0)
 b, L, k = 1, 40, 1
-alpha = sphere(pos=vec(-0.5*L + r1, b, 0), radius=r1, m=m1, q=q1, v=v0, color=c1, make_trail=True)
+t, dt = 0, 0.001
+
 particles = []
-for i in range(-5, 5):
-    sph = sphere(pos=vec(-0.5*L*i + r1, b, 0), radius=r1, m=m1, q=q1, v=v0, color=c1, make_trail=True)
+for i in range(-50, 50):
+    sph = sphere(pos=vec(-0.5 * L + r1, i * b, 0), radius=r1, m=m1, q=q1, v=v0, color=c1, make_trail=True)
     particles.append(sph)
 
-particles[2].pos.y += 12
+nucleus = sphere(pos = vec(0, 0, 0), radius = r2, m = m2, q = q2, color = c2)
 
+while(True):
+    for par in particles:
+        F = k * par.q * nucleus.q / par.pos.mag2 * par.pos.norm()
+        par.a = F / par.m
+        par.v += par.a * dt
+        par.pos += par.v * dt
 
-# particles.append(sphere(pos=vec(-0.5*L + r1, b, 0), radius=r1, m=m1, q=q1, v=v0, color=c1, make_trail=True))
-
-print(len(particles))
+    t += dt
+    if(t > 500):
+        break
 
 
