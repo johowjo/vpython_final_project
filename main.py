305841d4e2d49b10534dbba9057bb1ddecc81a04
johowjo <johowjo@gmail.com>
2024-06-07 19:08:40 +0800

    not much

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 from vpython import *
-import numpy as np
 import math
 
 r1, m1, q1 = 0.4, 4, 2
 r2, m2, q2 = 1, 197, 79
 v0 = vec(10, 0, 0)
 D, k = 20, 1
-b = 0.1 * r1
+b = 0.04
 t, dt = 0, 0.001
+b_range = 20
 
 num = 500
 
 
-def calc_angle(r):
-    # if r.x == 0:
-    #     if r.y > 0:
-    #         return np.pi / 2
-    #     else:
-    #         return -np.pi / 2
-    # elif r.y == 0:
-    #     if r.x > 0:
-    #         return 0
-    #     else:
-    #         return -np.pi
-    t = math.atan2(r.y, r.x)
+def calc_angle(r, i):
+    t = math.atan2(r.y - i * b_range / num, r.x)
     return t
 
 
 particles = []
+# y0 = []
 for i in range(-num, num):
     sph = sphere(
-        pos=vec(-D, i * b, 0), radius=r1, v=v0, color=color.red, make_trail=True
+        pos=vec(-D, i * b_range / num, 0),
+        radius=r1,
+        v=v0,
+        color=color.red,
+        make_trail=True,
     )
     particles.append(sph)
+    # y0.append(sph.pos.y)
 
 nucleus = sphere(pos=vec(0, 0, 0), radius=r2, color=color.yellow)
 
-while t < 5:
+while t < 4:
     for par in particles:
         F = k * q1 * q2 / par.pos.mag2 * par.pos.norm()
         par.a = F / m1
@@ -48,9 +44,11 @@ while t < 5:
 
 angles = []
 
-for par in particles:
+for i in range(-num, num):
+    par = particles[num + i]
     r = par.pos
-    angles.append(calc_angle(r))
+    # r.y -= i * b_range / num
+    angles.append(calc_angle(r, i))
 
 
 # print(len(particles))
@@ -62,13 +60,13 @@ angles.sort()
 # print(angles[999])
 
 scene = graph()
-graph = gcurve(color = color.blue, graph = scene)
+graph = gcurve(color=color.blue, graph=scene)
 
 
 p = 0
 for ang in angles:
     p += 1 / len(particles)
-    graph.plot(pos = (ang, p))
+    graph.plot(pos=(ang, p))
 
 #
 #
